e24be35f95394b65da2bf758d7fb3f5a25e06e9b
Rudra S <42294081+rudra404@users.noreply.github.com>
2021-08-04 10:04:59 +0800

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
-linkedin_username = 'rudra@kunger.at'
-linkedin_password = 'empowerfarmers'
+linkedin_username = 'example@example.com'
+linkedin_password = 'password1234'
 
 search_query = 'site:linkedin.com/in/ AND "Human Resources" AND "Hong Kong"'
 file_name = 'results_file.csv'
